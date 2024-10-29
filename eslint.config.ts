8dae8b058b5257fbea0bf4a09e59f2ce081c1d59
LiangYuxuan <lyx@lyx.es>
2024-10-29 11:32:26 +0800

    test: use eslint.config.ts

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,19 +1,25 @@
 import js from '@eslint/js';
-import core from './src/index.js';
+
+import core from './src/index.ts';
+
+import type { Linter } from 'eslint';
 
 export default [
     js.configs.recommended,
     ...core,
     {
         rules: {
-            'import/no-unused-modules': [
+            '@typescript-eslint/naming-convention': 'off',
+        },
+    },
+    {
+        rules: {
+            'import-x/no-unused-modules': [
                 'error',
                 {
                     ignoreExports: [
-                        'eslint.config.js',
-                        'src/index.js',
-                        'src/node.js',
-                        'src/typescript.js',
+                        'eslint.config.ts',
+                        'src/index.ts',
                     ],
                     missingExports: true,
                     unusedExports: true,
@@ -21,4 +27,4 @@ export default [
             ],
         },
     },
-];
+] satisfies Linter.Config[];
